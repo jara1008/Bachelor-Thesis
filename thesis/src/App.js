afb951d93290b5eb597eda6654b4c945fb02153e
huesserj <huesserj@student.ethz.ch>
2024-03-21 11:18:38 +0100

    start + a little background

---
+++ thesis/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './App.css';
+import Background from './components/background'
+
+function App() {
+  return (
+    <div className="App">
+      <Background className="background"></Background>
+    </div>
+  );
+}
+
+export default App;
---
+++ thesis/src/components/background.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './background.css';
+
+function Background() {
+  const [ellipses_white, setEllipses_white] = useState([
+    { top: '12%', left: '18%', width: '25px', height: '25px', blur: '5px' },
+    { top: '30%', left: '12%', width: '18px', height: '18px', blur: '4px' },
+    { top: '70%', left: '10%', width: '20px', height: '20px', blur: '4px' },
+    { top: '85%', left: '15%', width: '22px', height: '22px', blur: '5px' },
+    { top: '10%', left: '95%', width: '15px', height: '15px', blur: '4px' },
+    { top: '23%', left: '82%', width: '20px', height: '20px', blur: '4px' },
+    { top: '50%', left: '85%', width: '20px', height: '20px', blur: '4px' },
+    { top: '72%', left: '93%', width: '15px', height: '15px', blur: '4px' },
+    { top: '90%', left: '80%', width: '25px', height: '25px', blur: '5px' },
+  ]);
+
+  const [ellipses_blue, setEllipses_blue] = useState([
+    { top: '8%', left: '90%', width: '400px', height: '400px', blur: '100px' },
+    { top: '30%', left: '2%', width: '350px', height: '350px', blur: '100px' },
+    { top: '90%', left: '10%', width: '400px', height: '400px', blur: '100px' },
+    { top: '95%', left: '25%', width: '350px', height: '350px', blur: '100px' },
+    { top: '85%', left: '85%', width: '400px', height: '400px', blur: '100px' },
+  ]);
+
+  return (
+    <div className="main">
+      <div className="div">
+        {ellipses_blue.map((ellipse, index) => (
+            <div
+                key={index}
+                className="blue-ellipse"
+                style={{
+                '--ellipse-top': ellipse.top,
+                '--ellipse-left': ellipse.left,
+                '--ellipse-width': ellipse.width,
+                '--ellipse-height': ellipse.height,
+                '--ellipse-blur': ellipse.blur,
+                '--ellipse-radius': ellipse.borderRadius
+                }}
+            ></div>
+            ))}
+        {ellipses_white.map((ellipse, index) => (
+          <div
+            key={index}
+            className="white-ellipse"
+            style={{
+              '--ellipse-top': ellipse.top,
+              '--ellipse-left': ellipse.left,
+              '--ellipse-width': ellipse.width,
+              '--ellipse-height': ellipse.height,
+              '--ellipse-blur': ellipse.blur,
+              '--ellipse-radius': ellipse.borderRadius
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Background;
