b101f18bea48c76fec7659ea01c4fd8dd63e5c67
jara1008 <jara.huesser@gmail.com>
2024-04-02 12:07:50 +0200

    adjusted white box, clouds added

--- thesis/src/components/activity1.jsx
+++ thesis/src/components/activity1.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./activity1.css";
+import cloud from "../images/cloud.png";
 
 export const Box = () => {
     return (
-        <div className="box">
-            <div className="rectangle" />
+        <div className="container">
+            <div className="white-box" >
+                <img src={cloud} alt="Cloud" style={{position: "absolute", top: "40%", left: "0%", height: "58%", width: "48%"}} />
+                <img src={cloud} alt="Cloud" style={{position: "absolute", top: "40%", right: "1%", height: "58%", width: "48%"}} />
+            </div>
+            {/*<img src={star} className="svg-overlay" alt="Star SVG" />*/}
         </div>
     );
 };
