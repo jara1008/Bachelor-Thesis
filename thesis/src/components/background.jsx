1df7b5c9943dfd944b101433ff49c879b7bcaa6a
jara1008 <jara.huesser@gmail.com>
2024-04-18 16:09:51 +0200

    sparkles added to background

--- thesis/src/components/background.jsx
+++ thesis/src/components/background.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import './background.css';
+import sparkle from '../images/sparkle.svg';
 
 function Background() {
   const [ellipses_white, setEllipses_white] = useState([
-    { top: '12%', left: '10%', width: '25px', height: '25px', blur: '5px' },
-    { top: '30%', left: '6%', width: '18px', height: '18px', blur: '4px' },
-    { top: '70%', left: '4%', width: '20px', height: '20px', blur: '4px' },
-    { top: '85%', left: '8%', width: '22px', height: '22px', blur: '5px' },
+    { top: '12%', left: '17%', width: '25px', height: '25px', blur: '5px' },
+    { top: '30%', left: '10%', width: '18px', height: '18px', blur: '4px' },
+    { top: '60%', left: '12%', width: '18px', height: '18px', blur: '4px' },
+    { top: '78%', left: '18%', width: '22px', height: '22px', blur: '5px' },
     { top: '10%', left: '97%', width: '15px', height: '15px', blur: '4px' },
-    { top: '23%', left: '90%', width: '20px', height: '20px', blur: '4px' },
+    { top: '20%', left: '88%', width: '20px', height: '20px', blur: '4px' },
     { top: '50%', left: '95%', width: '22px', height: '22px', blur: '5px' },
-    { top: '72%', left: '95%', width: '15px', height: '15px', blur: '4px' },
-    { top: '90%', left: '90%', width: '25px', height: '25px', blur: '5px' },
+    { top: '75%', left: '85%', width: '15px', height: '15px', blur: '4px' },
+    { top: '90%', left: '93%', width: '25px', height: '25px', blur: '5px' },
   ]);
 
   const [ellipses_blue, setEllipses_blue] = useState([
@@ -20,6 +21,16 @@ function Background() {
     { top: '90%', left: '10%', width: '400px', height: '400px', blur: '100px' },
     { top: '95%', left: '25%', width: '350px', height: '350px', blur: '100px' },
     { top: '85%', left: '85%', width: '400px', height: '400px', blur: '100px' },
+  ]);
+
+  const [sparkles, setSparkles] = useState([
+    { top: '18%', left: '5%', size: '8%', opacity: '0.9' },
+    { top: '40%', left: '85%', size: '10%', opacity: '0.9' },
+    { top: '52%', left: '17%', size: '10%', opacity: '0.9' },
+    { top: '70%', left: '94%', size: '8%', opacity: '0.9' },
+    { top: '90%', left: '8%', size: '6%', opacity: '0.9' },
+    { top: '95%', left: '23%', size: '5%', opacity: '0.9' },
+    { top: '5%', left: '76%', size: '5%', opacity: '0.9' },
   ]);
 
   return (
@@ -52,6 +63,22 @@ function Background() {
               '--ellipse-radius': ellipse.borderRadius
             }}
           ></div>
+        ))}
+        {sparkles.map((sparkleItem, index) => (
+          <img
+            key={index}
+            src={sparkle}
+            alt="Sparkle"
+            style={{
+              position: 'absolute',
+              top: sparkleItem.top,
+              left: sparkleItem.left,
+              width: sparkleItem.size,
+              height: sparkleItem.size,
+              opacity: sparkleItem.opacity,
+              transform: 'translate(-50%, -50%)'
+            }}
+          />
         ))}
       </div>
     </div>
