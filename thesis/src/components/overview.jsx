940a5db2df3bd8820e86750ad25d6af0353c3f22
jara1008 <jara.huesser@gmail.com>
2024-04-18 15:03:17 +0200

    added: progress bar

--- thesis/src/components/overview.jsx
+++ thesis/src/components/overview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import "./overview.css";
 
@@ -15,8 +15,31 @@ function Overview() {
         { id: 9, top: '68%', left: '67%' }
     ])
 
+    const [dots, setDots] = useState([]);
+    const numberOfDots = 9; 
+
+    useEffect(() => {
+        const topOffset = 8; 
+        const bottomOffset = 90;
+        const newDots = Array.from({ length: numberOfDots }, (_, index) => ({
+            id: index,
+            top: `${topOffset + index * ((bottomOffset - topOffset) / (numberOfDots - 1))}%`
+        }));
+        setDots(newDots);
+    }, [numberOfDots]);
+
     return (
         <div className="overview">
+           <div className="progress-line">
+                {dots.map((dot) => (
+                    <div key={dot.id} style={{ 
+                        position: 'absolute', 
+                        top: dot.top, 
+                        left: '50%', 
+                        transform: 'translateX(-50%)'
+                    }} className="dot"></div>
+                ))}
+            </div>
             {boxes.map((box) => (
                 <Link to={box.path} key={box.id} style={{ position: 'absolute', top: box.top, left: box.left }}>
                     <div className="rectangle" />
