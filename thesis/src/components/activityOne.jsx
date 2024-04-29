129ed1dfd461a10be95decfff3fcdab9488e9c8e
jara1008 <jara.huesser@gmail.com>
2024-04-29 19:27:27 +0200

    activityOne fixed

--- thesis/src/components/activityOne.jsx
+++ thesis/src/components/activityOne.jsx
@@ -6,34 +6,42 @@ import { Link } from 'react-router-dom';
 import home_icon from '../images/home_icon.png';
 
 const leftCloudPositions = [
-    { top: 67, left: 20 }, { top: 45, left: 35 }, { top: 62, left: 6 }, 
-    { top: 73, left: 28 }, { top: 38, left: 25 }, { top: 50, left: 17 }, 
-    { top: 61, left: 38 } 
+    { top: 49, left: 20 }, { top: 27, left: 35 }, { top: 44, left: 6 }, 
+    { top: 55, left: 28 }, { top: 20, left: 25 }, { top: 32, left: 17 }, 
+    { top: 43, left: 38 } 
 ];
 const rightCloudPositions = [
-    { top: 51, left: 67 }, { top: 37, left: 75 }, { top: 72, left: 68 }, 
-    { top: 57, left: 90 }, { top: 62, left: 80 }, { top: 59, left: 57 }, 
-    { top: 45, left: 84 }  
+    { top: 33, left: 67 }, { top: 19, left: 75 }, { top: 54, left: 68 }, 
+    { top: 39, left: 90 }, { top: 44, left: 80 }, { top: 41, left: 57 }, 
+    { top: 27, left: 84 }  
 ];
 
 function ActivityOne() {
-
     const [allStars, setAllStars] = useState({ left: [], right: [] });
     const [firstPos, setFirstPos] = useState(null);
     const [secondPos, setSecondPos] = useState(null);
     const [lines, setLines] = useState([]);
     const firstPosRef = useRef(firstPos);
     const secondPosRef = useRef(secondPos);
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [displayCorrectness, setCorrectnessLabel] = useState(false);
+    const [roundCount, setRoundCount] = useState(1);
+    const [firstCloudCount, setFirstCloudCount] = useState(0);
+    const [secondCloudCount, setSecondCloudCount] = useState(0);
 
     useEffect(() => {
-        const firstCloudCount = Math.floor(Math.random() * leftCloudPositions.length) + 1;
-        const secondCloudCount = Math.floor(Math.random() * rightCloudPositions.length) + 1;
-
-        const starsInFirstCloud = generateStars(firstCloudCount, leftCloudPositions, 'left');
-        const starsInSecondCloud = generateStars(secondCloudCount, rightCloudPositions, 'right');
-
-        setAllStars({ left: starsInFirstCloud, right: starsInSecondCloud });
-    }, []);
+        setFirstCloudCount(Math.floor(Math.random() * leftCloudPositions.length) + 1);
+        setSecondCloudCount(Math.floor(Math.random() * rightCloudPositions.length) + 1);
+    }, []); 
+    
+    useEffect(() => {
+        if (leftCloudPositions.length > 0 && rightCloudPositions.length > 0) {
+            const starsInFirstCloud = generateStars(firstCloudCount, leftCloudPositions, 'left');
+            const starsInSecondCloud = generateStars(secondCloudCount, rightCloudPositions, 'right');
+    
+            setAllStars({ left: starsInFirstCloud, right: starsInSecondCloud });
+        }
+    }, [firstCloudCount, secondCloudCount, leftCloudPositions, rightCloudPositions]);
 
     const handleStarClick = (position) => {
         if (!firstPosRef.current) {
@@ -82,6 +90,77 @@ function ActivityOne() {
         ));
     };
 
+    const checkInput = () => {
+        setCorrectnessLabel(true);
+        const seenPositions = new Set();
+        
+        if (lines.length !== Math.min(firstCloudCount, secondCloudCount)) {
+            setLines([]);
+            return;
+        }
+
+        for (const line of lines) {
+            const { start, end } = line;
+            if (!((start.cloudSide === 'left' && end.cloudSide === 'right') || 
+                  (start.cloudSide === 'right' && end.cloudSide === 'left'))) {
+                console.log("Invalid line configuration:", line);
+                setLines([]);
+                return;
+            }
+    
+            const startPos = `start-${start.top}-${start.left}`;
+            const endPos = `end-${end.top}-${end.left}`;
+    
+            if (seenPositions.has(startPos) || seenPositions.has(endPos)) {
+                console.log("Duplicate positions found:", line);
+                setLines([]);
+                return;
+            }
+            seenPositions.add(startPos);
+            seenPositions.add(endPos);
+        }
+        setIsCorrect(true);
+        setRoundCount(roundCount + 1);
+    };
+
+    const handleNext = () => {
+        setFirstCloudCount(Math.floor(Math.random() * leftCloudPositions.length) + 1);
+        setSecondCloudCount(Math.floor(Math.random() * rightCloudPositions.length) + 1);
+
+        if (leftCloudPositions.length > 0 && rightCloudPositions.length > 0) {
+            const starsInFirstCloud = generateStars(firstCloudCount, leftCloudPositions, 'left');
+            const starsInSecondCloud = generateStars(secondCloudCount, rightCloudPositions, 'right');
+    
+            setAllStars({ left: starsInFirstCloud, right: starsInSecondCloud });
+        }
+        setIsCorrect(false);
+        setCorrectnessLabel(false);
+        setLines([]);
+    };
+
+    if (roundCount >= 5) {
+        // Message that the game is completed
+        return (
+            <div className="container">
+                <div className="white-box">
+                    <Link to={"/"}>
+                        <img src={home_icon} alt="home_icon" style={{ position: "absolute", top: "-8%", left: "95%" }} />
+                    </Link>
+                    <div className="congratulation-message">
+                        Gratulation! Du hast Level xy geschafft!
+                        { /* Add party icon */ }
+                    </div>
+                    <Link to={"/"}>
+                        <button className='button'
+                            style={{ top: '85%', left: '50%', width: '30%' }} >
+                            zur Übersicht
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="container" >
             <div className="white-box" >
@@ -90,9 +169,9 @@ function ActivityOne() {
                 </Link>
                 <span className="text-wrapper">Verbinde die Sterne miteinander:</span>
                 <div style={{ position: "relative", height: "100%", width: "100%" }}>
-                    <img src={cloud} alt="Cloud" style={{ position: "absolute", top: "30%", left: "0%", height: "58%", width: "48%" }} />
+                    <img src={cloud} alt="Cloud" style={{ position: "absolute", top: "12%", left: "0%", height: "58%", width: "48%" }} />
                     {allStars.left}
-                    <img src={cloud} alt="Cloud" style={{ position: "absolute", top: "30%", right: "1%", height: "58%", width: "48%" }} />
+                    <img src={cloud} alt="Cloud" style={{ position: "absolute", top: "12%", right: "1%", height: "58%", width: "48%" }} />
                     {allStars.right}
                     <svg style={{ position: "absolute", top: 0, left: 0, height: "100%", width: "100%", pointerEvents: "none" }}>
                         {lines.map((line, index) => (
@@ -103,6 +182,12 @@ function ActivityOne() {
                         ))}
                     </svg>
                 </div>
+                {isCorrect && displayCorrectness && <div className="correctness-label-correct1">Richtig!</div>}
+                {!!!isCorrect && displayCorrectness && <div className="correctness-label-false1">Versuche es nochmals!</div>}
+                <button onClick={isCorrect ? handleNext : checkInput} className="button" 
+                    style={{ top: '88%', left: '85%' }} >
+                    {isCorrect ? "Weiter" : "Prüfen"}
+                </button>
             </div>
         </div>
     );
