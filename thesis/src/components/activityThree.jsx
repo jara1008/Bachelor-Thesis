045f2471d11e2ac6fc8838fa2431b1a7941724f7
jara1008 <jara.huesser@gmail.com>
2024-05-03 16:50:25 +0200

    activtiyThree updated

--- thesis/src/components/activityThree.jsx
+++ thesis/src/components/activityThree.jsx
@@ -7,11 +7,33 @@ import congratulation_icon from '../images/congratulation_icon.png';
 
 function ActivityThree() {
     const [roundCount, setRoundCount] = useState(1);
-    const [leftCoinsTen, setLeftCoinsTen] = useState(Math.floor(Math.random() * 4) + 1); 
-    const [leftCoinsOne, setLeftCoinsOne] = useState(Math.floor(Math.random() * 4) + 1); 
-    const [rightCoinsTen, setRightCoinsTen] = useState(Math.floor(Math.random() * 4) + 1); 
-    const [rightCoinsOne, setRightCoinsOne] = useState(Math.floor(Math.random() * 4) + 1); 
+    const [leftCoinsTen, setLeftCoinsTen] = useState(Math.floor(Math.random() * 2) + 1); 
+    const [leftCoinsOne, setLeftCoinsOne] = useState(Math.floor(Math.random() * 6) + 1); 
+    const [rightCoinsTen, setRightCoinsTen] = useState(Math.floor(Math.random() * 2) + 1); 
+    const [rightCoinsOne, setRightCoinsOne] = useState(Math.floor(Math.random() * 6) + 1); 
 
+    function CoinRow({ coinsTen, coinsOne, type }) {
+        return (
+            <div className="coin-stack">
+                {Array.from({ length: coinsTen }, (_, i) => (
+                    <div key={`ten-${i}`} className={`coin ${type}-coin`}
+                    onClick={() => handleCoinClick(type)}>10</div>
+                ))}
+                {Array.from({ length: coinsOne }, (_, i) => (
+                    <div key={`one-${i}`} className={`coin ${type}-coin`}
+                    onClick={() => handleCoinClick(type)}>1</div>
+                ))}
+            </div>
+        );
+    }
+
+    function handleCoinClick(type) {
+        if (type === 'lower') {
+            return;
+        }
+        //add logic to make coins dis- and reappear upon clicking on the upper coin
+    }
+      
     if (roundCount >= 5) {
         // Message that the game is completed
         return (
@@ -43,23 +65,16 @@ function ActivityThree() {
                 </Link>
                 <span className='text-wrapper'>TODO:</span>
                 <div className="coin-row">
-                    <div className="left-coins">
-                        {Array.from({ length: leftCoinsTen }, (_, i) => (
-                            <div key={i} className='coin'>10</div>
-                        ))}
-                        {Array.from({ length: leftCoinsOne }, (_, i) => (
-                            <div key={i} className='coin'>1</div>
-                        ))}
-                    </div>
-                    <span className='text-wrapper-questionmark'>?</span>
-                    <div className="right-coins">
-                        {Array.from({ length: rightCoinsTen }, (_, i) => (
-                            <div key={i} className='coin'>10</div>
-                        ))}
-                        {Array.from({ length: rightCoinsOne }, (_, i) => (
-                            <div key={i} className='coin'>1</div>
-                        ))}
-                    </div>
+                    <CoinRow coinsTen={leftCoinsTen} coinsOne={leftCoinsOne} type='upper' />
+                    <span className='text-wrapper-abs' style={{ '--left': '50%' }}>?</span>
+                    <CoinRow coinsTen={rightCoinsTen} coinsOne={rightCoinsOne} type='upper' />
+                </div>
+                <span className='text-wrapper-abs' style={{ '--top': '45%', '--left': '25%' }}>↓</span>
+                <span className='text-wrapper-abs' style={{ '--top': '45%', '--left': '75%' }}>↓</span>
+                <div className="coin-row" style={{ '--top': '38%' }}>
+                    <CoinRow coinsTen={leftCoinsTen} coinsOne={leftCoinsOne} type='lower' />
+                    <input className='info-input'></input>
+                    <CoinRow coinsTen={rightCoinsTen} coinsOne={rightCoinsOne} type='lower' />
                 </div>
             </div>
         </div>
