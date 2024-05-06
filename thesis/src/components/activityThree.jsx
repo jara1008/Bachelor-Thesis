10096ae200860e42ed03e25c39ad099e123f6ae3
jara1008 <jara.huesser@gmail.com>
2024-05-06 11:22:51 +0200

    activityThree: added coin logic

--- thesis/src/components/activityThree.jsx
+++ thesis/src/components/activityThree.jsx
@@ -1,41 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './activityThree.css';
 import { Link } from 'react-router-dom';
 import home_icon from '../images/home_icon.png';
-import congratulation_icon from '../images/congratulation_icon.png';
-
+import congratulation_icon from '../images/congratulation_icon.png'; // Assuming this will be used later
 
 function ActivityThree() {
     const [roundCount, setRoundCount] = useState(1);
     const [leftCoinsTen, setLeftCoinsTen] = useState(Math.floor(Math.random() * 2) + 1); 
     const [leftCoinsOne, setLeftCoinsOne] = useState(Math.floor(Math.random() * 6) + 1); 
     const [rightCoinsTen, setRightCoinsTen] = useState(Math.floor(Math.random() * 2) + 1); 
-    const [rightCoinsOne, setRightCoinsOne] = useState(Math.floor(Math.random() * 6) + 1); 
+    const [rightCoinsOne, setRightCoinsOne] = useState(Math.floor(Math.random() * 6) + 1);
+    const [activeCoins, setActiveCoins] = useState(new Set());
+    const [leftVisibility, setLeftVisibility] = useState({
+        tens: Array(leftCoinsTen).fill(true),
+        ones: Array(leftCoinsOne).fill(true)
+    });
+    const [rightVisibility, setRightVisibility] = useState({
+        tens: Array(rightCoinsTen).fill(true),
+        ones: Array(rightCoinsOne).fill(true)
+    });
 
-    function CoinRow({ coinsTen, coinsOne, type }) {
+    function CoinRowUpper({ coinsTen, coinsOne, type }) {
         return (
             <div className="coin-stack">
-                {Array.from({ length: coinsTen }, (_, i) => (
-                    <div key={`ten-${i}`} className={`coin ${type}-coin`}
-                    onClick={() => handleCoinClick(type)}>10</div>
+                {Array.from({ length: coinsTen }, (_, i) => {
+                    const coinKey = `${type}-tens-${i}`;
+                    const isActive = activeCoins.has(coinKey)
+                    const className = `coin ${type}-coin ${isActive ? 'active-coin' : ''}`;
+                    return (
+                        <div key={`ten-${i}`} className={className}
+                            onClick={() => handleCoinClick(type, 'tens', i)}>10</div>
+                    );
+                })}
+                {Array.from({ length: coinsOne }, (_, i) => {
+                    const coinKey = `${type}-ones-${i}`;
+                    const isActive = activeCoins.has(coinKey)
+                    const className = `coin ${type}-coin ${isActive ? 'active-coin' : ''}`;
+                    return (
+                        <div key={`one-${i}`} className={className}
+                            onClick={() => handleCoinClick(type, 'ones', i)}>1</div>
+                    );
+                })}
+            </div>
+        );
+    }    
+
+    function CoinRowLower({ coinsTen, coinsOne, type, visibility }) {
+        return (
+            <div className="coin-stack">
+                {Array.from({ length: coinsTen }, (_, i) => visibility.tens[i] && (
+                    <div key={`ten-${i}`} className={`coin ${type}-coin`}>10</div>
                 ))}
-                {Array.from({ length: coinsOne }, (_, i) => (
-                    <div key={`one-${i}`} className={`coin ${type}-coin`}
-                    onClick={() => handleCoinClick(type)}>1</div>
+                {Array.from({ length: coinsOne }, (_, i) => visibility.ones[i] && (
+                    <div key={`one-${i}`} className={`coin ${type}-coin`}>1</div>
                 ))}
             </div>
         );
     }
 
-    function handleCoinClick(type) {
-        if (type === 'lower') {
-            return;
+    function handleCoinClick(type, denomination, index) {
+        const coinKey = `${type}-${denomination}-${index}`;
+
+        setActiveCoins(prevActiveCoins => {
+            const newActiveCoins = new Set(prevActiveCoins);
+            if (leftVisibility[denomination][index] != rightVisibility[denomination][index]) {
+                alert("WRONG!"); //BETTER: make a disclaimer!
+            }
+            else if (newActiveCoins.has(coinKey)) {
+                newActiveCoins.delete(coinKey);
+            } else {
+                newActiveCoins.add(coinKey);
+            }
+            return newActiveCoins;
+        });
+
+        if (leftVisibility[denomination][index] === rightVisibility[denomination][index]) {
+            const updateVisibility = (visibility, setVisibility) => {
+                const newVisibility = {...visibility};
+                newVisibility[denomination][index] = !newVisibility[denomination][index];
+                setVisibility(newVisibility);
+            };
+
+            if (type === 'upper') {
+                updateVisibility(leftVisibility, setLeftVisibility);
+                updateVisibility(rightVisibility, setRightVisibility);
+            }
         }
-        //add logic to make coins dis- and reappear upon clicking on the upper coin
     }
-      
+
     if (roundCount >= 5) {
-        // Message that the game is completed
         return (
             <div className="container">
                 <div className="white-box">
@@ -44,11 +97,9 @@ function ActivityThree() {
                     </Link>
                     <div className="congratulation-message">
                         Gratulation! Du hast Level xy geschafft!
-                        { /* Add party icon */ }
                     </div>
                     <Link to={"/"}>
-                        <button className='button'
-                            style={{ top: '85%', left: '50%', width: '30%' }} >
+                        <button className='button' style={{ top: '85%', left: '50%', width: '30%' }}>
                             zur Übersicht
                         </button>
                     </Link>
@@ -63,18 +114,18 @@ function ActivityThree() {
                 <Link to={"/"}>
                     <img src={home_icon} alt="home_icon" style={{ position: "absolute", top: "-8%", left: "95%" }} />
                 </Link>
-                <span className='text-wrapper'>TODO:</span>
+                <div className='text-wrapper'>TODO:</div>
                 <div className="coin-row">
-                    <CoinRow coinsTen={leftCoinsTen} coinsOne={leftCoinsOne} type='upper' />
+                    <CoinRowUpper coinsTen={leftCoinsTen} coinsOne={leftCoinsOne} type='upper' visibility={leftVisibility} setVisibility={setLeftVisibility} />
                     <span className='text-wrapper-abs' style={{ '--left': '50%' }}>?</span>
-                    <CoinRow coinsTen={rightCoinsTen} coinsOne={rightCoinsOne} type='upper' />
+                    <CoinRowUpper coinsTen={rightCoinsTen} coinsOne={rightCoinsOne} type='upper' visibility={rightVisibility} setVisibility={setRightVisibility} />
                 </div>
                 <span className='text-wrapper-abs' style={{ '--top': '45%', '--left': '25%' }}>↓</span>
                 <span className='text-wrapper-abs' style={{ '--top': '45%', '--left': '75%' }}>↓</span>
                 <div className="coin-row" style={{ '--top': '38%' }}>
-                    <CoinRow coinsTen={leftCoinsTen} coinsOne={leftCoinsOne} type='lower' />
+                    <CoinRowLower coinsTen={leftCoinsTen} coinsOne={leftCoinsOne} type='lower' visibility={leftVisibility} setVisibility={setLeftVisibility} />
                     <input className='info-input'></input>
-                    <CoinRow coinsTen={rightCoinsTen} coinsOne={rightCoinsOne} type='lower' />
+                    <CoinRowLower coinsTen={rightCoinsTen} coinsOne={rightCoinsOne} type='lower' visibility={rightVisibility} setVisibility={setRightVisibility} />
                 </div>
             </div>
         </div>
