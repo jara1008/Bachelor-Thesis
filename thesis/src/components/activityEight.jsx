20ab733f680c925ccb7968533d6cd2275e9aaaa2
jara1008 <jara.huesser@gmail.com>
2024-05-07 17:00:43 +0200

    added: car and colored dots

--- thesis/src/components/activityEight.jsx
+++ thesis/src/components/activityEight.jsx
@@ -3,7 +3,8 @@ import './activityEight.css';
 import { Link } from 'react-router-dom';
 import home_icon from '../images/home_icon.png';
 import congratulation_icon from '../images/congratulation_icon.png';
-import tree from '../images/tree.png'
+import tree from '../images/tree.png';
+import car from '../images/car_large.png';
 
 function ActivityEight() {
     const [numbers, setNumbers] = useState({ largeNum: 0, smallNum: 0 });
@@ -13,22 +14,39 @@ function ActivityEight() {
     const [displayCorrectness, setCorrectnessLabel] = useState(false);
     const [position, setPosition] = useState(Math.random(-6, 8));
 
-    const leftNumbers = [6, 5, 4, 3, 2, 1, 0];
-    const rightNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    const leftNumbers = [7, 6, 5, 4, 3, 2, 1];
+    const rightNumbers = [1, 2, 3, 4, 5, 6, 7];
+
+    const [carPosition, setCarPosition] = useState(Math.floor(Math.random() * (leftNumbers.length + rightNumbers.length + 1)));
+    console.log(carPosition);
 
     const renderNumberLine = () => (
         <div>
             <img src={tree} style={{
-                position: 'absolute',
-                top: '55%',
-                left: '38%',
+                position: 'relative',
+                top: '10vh',
+                left: '50%',
+                width: '40px',
+                height: '40px',
+                transform:'translate(-50%)',
+            }}></img>
+            <img src={car} style={{ 
+                left: `${2 + (carPosition * 6.1)}%`, //TODO: make resizable...
+                position: 'relative',
+                top: '10.2vh',
+                width: '6%',
+                height: '3%',
+                transform:'translateX(-50%)',
             }}></img>
             <div className="number-line">
                 {leftNumbers.map((num, index) => (
-                    <div key={`left-${index}`} className="number-dot">{num}</div>
+                    <div key={`left-${index}`} className="number-dot red-dot">
+                        {num}
+                    </div>
                 ))}
+                <div key={ 'zero' } className="number-dot">0</div>
                 {rightNumbers.map((num, index) => (
-                    <div key={`right-${index}`} className="number-dot">{num}</div>
+                    <div key={`right-${index}`} className="number-dot blue-dot">{num}</div>
                 ))}
             </div>
         </div>
